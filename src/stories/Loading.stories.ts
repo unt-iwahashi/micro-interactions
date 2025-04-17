d16c0fb1dd96a84c65b45f9ede5a97849c8bea4c
Iwahashi Aki <iwahashi@un-t.com>
2025-04-17 17:29:58 +0900

    remove: 不要なファイル削除

---
+++ src/stories/Loading.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Loading01 as LoadingComponent } from '@/components/loading/Loading01';
+
+const meta = {
+  title: 'Example/Loading',
+  component: LoadingComponent,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof LoadingComponent>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const Loading01: Story = {
+  args: {
+    label: 'Loading',
+  },
+};
