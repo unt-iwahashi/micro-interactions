105fe639d4cc6f41c4229478bc3a6ab697adeedb
Iwahashi Aki <iwahashi@un-t.com>
2025-04-17 17:29:44 +0900

    add: plugin-react追加

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/_partials/_all" as *;`, // 必要に応じて
+      },
+    },
+  },
+  plugins: [react()],
+});
